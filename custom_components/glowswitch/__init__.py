f900d668f5ef9dde658b858763e50b743b6a76e4
Duckida <61168649+duckida@users.noreply.github.com>
2025-05-16 18:56:23 +0100

    Update __init__.py

--- custom_components/glowswitch/__init__.py
+++ custom_components/glowswitch/__init__.py
@@ -34,8 +34,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         raise ConfigEntryNotReady(f"{address} is not advertising state")
 
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
-    await hass.config_entries.async_forward_entry_setups(entry, "binary_sensor")
-    await hass.config_entries.async_forward_entry_setups(entry, "light")
+    await hass.config_entries.async_forward_entry_setups(entry, ["binary_sensor", "light"])
+
 
     return True
 
